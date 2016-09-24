143cab6e5b9ceef40a5044e715e8e3fdc83e8393
Alejandro Castillejo <castillejo.alejandro@gmail.com>
2016-09-24 15:49:34 -0700

    Minor tweaks

--- responsive.js
+++ responsive.js
@@ -1,31 +1,64 @@
 function responsive()
 {
-		//Conserve grid cell's height-width ratio
+	//Conserve grid cell's height-width ratio
 	var cellWidth = $('.cell').width();
-	console.log(cellWidth);
-	$('.cell').css({
+
+	//For apps
+	$('.app').children().css({
 	    'height': 1.778667*cellWidth + 'px'
 	});
 
-	//Conserve grid height based on cells height. Assuming above 1001px screenWidth there will be 3 elements, below, 2 elements.
-	var cellHeight = $('.cell').height();
-	var elements = $(".grid > div").length;
-	var screenWidth = $(document).width();
-	var divider = 3;
-	if (screenWidth < 480){
-		divider = 1;
-	} else if (screenWidth < 1001){
-		divider = 2;
-	}
-	var multiplier = Math.ceil(elements/divider);
+	//For projects
+	$('.project').children().css({
+	    'height': 0.562701*cellWidth + 'px'
+	});
 
+	//Conserve grid height based on cells height. Assuming above 1001px screenWidth there will be 3 elements, below, 2 elements.
+	var screenWidth = $(document).width();
+	var divider = 0;
 	var paddingTop = parseInt($('.grid').css('padding-top'));
 	var paddingBottom = parseInt($('.grid').css('padding-bottom'));
-	var padding = paddingBottom + paddingTop;
 
-	$('.grid').css({
-	    'height': multiplier*cellHeight + padding + 'px'
+	$(".grid").each(function(){
+
+		var cellHeight = $(this).children().height();
+		var elements = $(this).children().length;
+		
+		if (screenWidth < 480){
+			divider = 1;
+		} else if (screenWidth < 1001){
+			divider = 2;
+		} else {
+			divider = 3;
+		}
+		var multiplier = Math.ceil(elements/divider);
+
+		$(this).css({
+		    'height': multiplier*cellHeight + paddingTop + paddingBottom + 'px'
+		});
+
 	});
+
+	//Set height of cellContent
+	var marginTopCellContent = parseInt($('.cellContent').css('margin-top'));
+	var marginBottomCellContent = parseInt($('.cellContent').css('margin-bottom'));
+	var cellAppHeight = $('.app').children().height();
+	var cellProjectHeight = $('.project').children().height();
+
+	$(".cell").each(function(){
+
+		if($(this).parent().is('.app')){
+			$(this).children().css({
+				    'height': cellAppHeight - marginTopCellContent - marginBottomCellContent + 'px'
+			});
+		} else if ($(this).parent().is('.project')){
+			$(this).children().css({
+				    'height': cellProjectHeight - marginTopCellContent - marginBottomCellContent + 'px'
+			});
+		}
+
+	});
+
 }
 
 //Call for the first time
